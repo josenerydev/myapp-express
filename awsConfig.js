6958ae4d3e9bf21d46b69fbfb005fb250f9a8ffd
Jose Nery <josenerydev@gmail.com>
2023-08-19 17:15:29 -0300

    feat(awsConfig): import getSecrets and update getDatabaseConfig
    - Import getSecrets function from secrets module
    - Await the getSecrets function and assign the result to awsSecrets variable
    - Log the value of awsSecrets using console.log
    - Update the secrets object using the values from awsSecrets

--- awsConfig.js
+++ awsConfig.js
@@ -1,15 +1,26 @@
+const getSecrets = require('./secrets'); 
+
 async function getDatabaseConfig() {
     let secrets;
 
     if (process.env.USE_AWS_SECRETS === 'true') {
-        secrets = await getSecrets();
+        const awsSecrets = await getSecrets();
+        console.log(awsSecrets);
+        
+        secrets = {
+            host: awsSecrets.DB_HOST,
+            user: awsSecrets.DB_USER,
+            password: awsSecrets.DB_PASSWORD,
+            database: awsSecrets.DB_NAME,
+            port: parseInt(awsSecrets.DB_PORT, 10) || 5432
+        };
     } else {
         secrets = {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
-            port: process.env.DB_PORT || 5432
+            port: parseInt(process.env.DB_PORT, 10) || 5432
         };
     }
 
